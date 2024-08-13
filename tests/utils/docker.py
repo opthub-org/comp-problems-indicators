a1e1620f09b5ba56a13a1f153e6ab39c86aca1e5
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2024-08-14 02:24:27 +0900

    chore: ignore Ruff warning

--- tests/utils/docker.py
+++ tests/utils/docker.py
@@ -10,8 +10,8 @@ def build_image_on_subprocess(image_type: str, name: str) -> None:
         image_type (str): problem or indicator
         name (str): problem or indicator name
     """
-    subprocess.run(["make", f"build-{image_type}", f"NAME={name}"], check=True)  # Build the image
-    subprocess.run(["docker", "image", "prune", "-f"], check=True)  # Delete <none> images
+    subprocess.run(["make", f"build-{image_type}", f"NAME={name}"], check=True)  # Build the image  # noqa: S603, S607
+    subprocess.run(["docker", "image", "prune", "-f"], check=True)  # Delete <none> images  # noqa: S603, S607
 
 
 def build_image(image_type: str, name: str) -> str:
